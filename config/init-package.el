fa08ef578487675c683c36f6699312e8e6bb8b2f
Antoine Martin <antoine97.martin@gmail.com>
2018-07-21 23:29:08 +0200

    Add melpa unstable repos too

--- config/init-package.el
+++ config/init-package.el
@@ -3,12 +3,15 @@
 (add-to-list 'package-archives
              '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 (add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives
              '("org" . "https://orgmode.org/elpa/") t)
 
 (setq package-archive-priorities
       '(("melpa-stable" . 10)
         ("org" . 10)
-        ("gnu" . 0)))
+        ("gnu" . 5)
+        ("melpa" . 0)))
 
 ;; Fetch package list on first init
 (when (not package-archive-contents)
