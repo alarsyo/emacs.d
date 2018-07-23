cf014cb898928d25e2fcec3f12d41a43c9cf1b85
Antoine Martin <antoine97.martin@gmail.com>
2018-07-23 17:03:51 +0200

    Move package initialization after setting repos

--- config/init-package.el
+++ config/init-package.el
@@ -1,11 +1,11 @@
 (require 'package)
-(package-initialize)
 (add-to-list 'package-archives
              '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 (add-to-list 'package-archives
              '("melpa" . "https://melpa.org/packages/") t)
 (add-to-list 'package-archives
              '("org" . "https://orgmode.org/elpa/") t)
+(package-initialize)
 
 ;; Prioritize stable and org repo over gnu/melpa unstable
 (setq package-archive-priorities
