744e55f3b5a9644b295743e58b6637e21aaf92db
Antoine Martin <antoine97.martin@gmail.com>
2018-08-01 12:05:57 +0200

    Disable finddir in projectile

--- config/init-projectile.el
+++ config/init-projectile.el
@@ -1,10 +1,6 @@
 ;; Folders where to look for projects
 (setq projectile-project-search-path
-      (list (expand-file-name "code" my-homedir)))
-
-;; Open dired when opening project
-(setq projectile-switch-project-action #'projectile-find-dir)
-(setq projectile-find-dir-includes-top-level t) ; include top level in list
+      (list (expand-file-name "code/" my-homedir)))
 
 (projectile-mode t)
 
