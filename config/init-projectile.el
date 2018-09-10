d95a6476ef720e1eebcad6e06e2fc5cd97ecaee7
Antoine Martin <antoine4.martin@epita.fr>
2018-09-10 12:00:20 +0200

    Open project in dired mode

--- config/init-projectile.el
+++ config/init-projectile.el
@@ -5,4 +5,6 @@
 (projectile-mode t)
 (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
 
+(setq projectile-switch-project-action #'projectile-dired)
+
 (provide 'init-projectile)
