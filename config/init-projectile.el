e34e3fa4c6ea18b52b400015c957a0b22aa960e8
Antoine Martin <antoine4.martin@epita.fr>
2018-09-17 09:17:01 +0200

    Remove folders for projectile

--- config/init-projectile.el
+++ config/init-projectile.el
@@ -1,7 +1,3 @@
-;; Folders where to look for projects
-(setq projectile-project-search-path
-      (list (expand-file-name "code/" my-homedir)))
-
 (projectile-mode t)
 (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
 
