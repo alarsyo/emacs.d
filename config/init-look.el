59c24a1fffeb4425819bd66a3131e53757c7d909
Antoine Martin <antoine97.martin@gmail.com>
2018-10-11 19:39:09 +0200

    Add Hack font

--- config/init-look.el
+++ config/init-look.el
@@ -1,6 +1,8 @@
 (load-theme 'base16-default-dark t)
 (global-hl-line-mode t)
 
-(set-face-attribute 'default nil :height 100)
+(set-face-attribute 'default nil
+                    :family "Hack"
+                    :height 100)
 
 (provide 'init-look)
