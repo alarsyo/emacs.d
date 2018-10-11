8bce11bc29dbbb228d218ca358e531a9ac0f48f7
Antoine Martin <antoine97.martin@gmail.com>
2018-10-11 20:44:33 +0200

    Switch theme

--- config/init-look.el
+++ config/init-look.el
@@ -1,4 +1,11 @@
-(load-theme 'base16-default-dark t)
+(load-theme 'base16-onedark t)
+(defvar my/base16-colors base16-onedark-colors)
+(setq evil-emacs-state-cursor   `(,(plist-get my/base16-colors :base0D) box)
+      evil-insert-state-cursor  `(,(plist-get my/base16-colors :base0D) bar)
+      evil-motion-state-cursor  `(,(plist-get my/base16-colors :base0E) box)
+      evil-normal-state-cursor  `(,(plist-get my/base16-colors :base0B) box)
+      evil-replace-state-cursor `(,(plist-get my/base16-colors :base08) bar)
+      evil-visual-state-cursor  `(,(plist-get my/base16-colors :base09) box))
 (global-hl-line-mode t)
 
 (set-face-attribute 'default nil
