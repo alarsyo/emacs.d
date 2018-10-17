848a538e8c0747ddcc7e6e0e5942e80b0a56bebe
Antoine Martin <antoine4.martin@epita.fr>
2018-10-17 19:02:51 +0200

    Change font depending on host

--- config/init-look.el
+++ config/init-look.el
@@ -6,4 +6,9 @@
                     :family "Hack"
                     :height 105)
 
+(add-to-list 'default-frame-alist
+             (if (string= (user-login-name) "ma_9")
+                 '(font . "Hack-10")
+               '(font . "Hack-11")))
+
 (provide 'init-look)
