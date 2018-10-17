d7f9e7245136639c4311a9072a07b4276d5e11d9
Antoine Martin <antoine97.martin@gmail.com>
2018-10-17 16:15:49 +0200

    Use official dracula theme

--- config/init-look.el
+++ config/init-look.el
@@ -1,11 +1,5 @@
-(load-theme 'base16-onedark t)
-(defvar my/base16-colors base16-onedark-colors)
-(setq evil-emacs-state-cursor   `(,(plist-get my/base16-colors :base0D) box)
-      evil-insert-state-cursor  `(,(plist-get my/base16-colors :base0D) bar)
-      evil-motion-state-cursor  `(,(plist-get my/base16-colors :base0E) box)
-      evil-normal-state-cursor  `(,(plist-get my/base16-colors :base0B) box)
-      evil-replace-state-cursor `(,(plist-get my/base16-colors :base08) bar)
-      evil-visual-state-cursor  `(,(plist-get my/base16-colors :base09) box))
+(load-theme 'dracula t)
+
 (global-hl-line-mode t)
 
 (set-face-attribute 'default nil
