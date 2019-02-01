7fa0f577507f80f1c22d9a22b1349353ae74d7d9
Antoine Martin <antoine97.martin@gmail.com>
2019-02-01 22:39:19 +0100

    golang: larger column limit

--- config/init-golang.el
+++ config/init-golang.el
@@ -1,4 +1,9 @@
-(setq gofmt-command "goimports")
 (add-hook 'before-save-hook 'gofmt-before-save)
+
+(add-hook 'go-mode-hook
+          (lambda ()
+            (setq-local whitespace-line-column 120)))
+
+(setq gofmt-command "goimports")
 
 (provide 'init-golang)
